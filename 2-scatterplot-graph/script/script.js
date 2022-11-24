03d457b0bac2f916f92ac19e4b3d3b8515aec211
arq-gabo <jgabrielguerral@gmail.com>
2022-11-23 23:50:15 -0400

    Add axis properties

--- 2-scatterplot-graph/script/script.js
+++ 2-scatterplot-graph/script/script.js
@@ -6,20 +6,32 @@ async function getData() {
 
   try {
     let res = await fetch(apiUrl);
-    let data = await res.json();
-    console.log(data);
+    return await res.json();
   } catch (e) {
     console.log(e);
   }
 }
 
 async function renderData() {
-  getData();
+  //parsed api data for used in create graph
+  let data = await getData();
+
   // Create svg using d3.js library
 
   // Initial values
-  const w = 700;
-  const h = 500;
+  const margin = 50;
+  const w = data.length * 20 + margin * 2; //800px
+  const h = 550;
+  const graphW = w - margin * 2;
+  const graphH = h - margin * 2;
+  const minYear = d3.min(data.map((val) => val.Year)) - 1;
+  const maxYear = d3.max(data.map((val) => val.Year)) + 1;
+
+  //initial values to y axis
+  const specifier = "%M:%S";
+  const parsedTime = data.map((d) => d3.timeParse(specifier)(d.Time));
+  const minTime = d3.min(parsedTime);
+  const maxTime = d3.max(parsedTime);
 
   //Create svg
   const svg = d3
@@ -27,6 +39,22 @@ async function renderData() {
     .append("svg")
     .attr("width", w)
     .attr("height", h);
+
+  //Create X horizontal axis
+  const xAxis = d3.scaleLinear().domain([minYear, maxYear]).range([0, graphW]);
+
+  svg
+    .append("g")
+    .attr("transform", `translate(${margin}, ${margin + graphH})`)
+    .call(d3.axisBottom(xAxis).tickFormat(d3.format("")));
+
+  //Create Y Vertical axis
+  const Yaxis = d3.scaleTime().domain([minTime, maxTime]).range([0, graphH]);
+
+  svg
+    .append("g")
+    .attr("transform", `translate(${margin}, ${margin})`)
+    .call(d3.axisLeft(Yaxis).tickFormat(d3.timeFormat(specifier)));
 }
 
 renderData();
