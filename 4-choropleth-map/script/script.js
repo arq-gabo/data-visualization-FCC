5ab5f38de9f109afc03f519738a9be73ec77babf
arq-gabo <jgabrielguerral@gmail.com>
2022-12-09 10:09:32 -0400

    feature maps

--- 4-choropleth-map/script/script.js
+++ 4-choropleth-map/script/script.js
@@ -7,6 +7,18 @@ const countyData =
 const w = 960;
 const h = 600;
 
+colorScale = [
+  "#e5f4e1",
+  "#c7e9c0",
+  "#a1d89a",
+  "#74c577",
+  "#41ab5d",
+  "#238a44",
+  "#016d2d",
+];
+
+const getColor = (val) => {};
+
 const path = d3.geoPath();
 
 const svg = d3
@@ -28,5 +40,15 @@ function ready([us, edu]) {
     .data(topojson.feature(us, us.objects.counties).features)
     .enter()
     .append("path")
-    .attr("d", path);
+    .attr("d", path)
+    .attr("class", "counties");
+
+  svg
+    .append("g")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("class", "states");
 }
