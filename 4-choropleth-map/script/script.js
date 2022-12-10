68eb08a6a02d43aa30392930770d8f56ddaa2a74
arq-gabo <jgabrielguerral@gmail.com>
2022-12-10 18:03:24 -0400

    Finish Color Map

--- 4-choropleth-map/script/script.js
+++ 4-choropleth-map/script/script.js
@@ -5,19 +5,27 @@ const countyData =
 
 let promises = [d3.json(countyData), d3.json(educationData)];
 
+const legendSize = document.getElementById("choropleth-legend");
+
+let percentageEducation = 0;
+
 Promise.all(promises)
   .then(ready)
   .catch((e) => console.log(e));
 
 function ready([us, edu]) {
   // ----------------------- Initial Values ----------------------------------
-  const w = 960;
+
+  const w = 950;
   const h = 600;
+
+  const wLeg = legendSize.clientWidth;
+  const hLeg = legendSize.clientHeight;
 
   const path = d3.geoPath();
 
-  const minPercentBachelor = d3.min(edu.map((val) => val.bachelorsOrHigher));
-  const maxPercentBachelor = d3.max(edu.map((val) => val.bachelorsOrHigher));
+  const maxPercentBachelor =
+    Math.ceil(d3.max(edu.map((val) => val.bachelorsOrHigher)) / 10) * 10;
 
   // ----------------------- Create SVG ---------------------------------------
   const svg = d3
@@ -26,32 +34,44 @@ function ready([us, edu]) {
     .attr("width", w)
     .attr("height", h);
 
+  // --------------------- Create Legend ---------------------------------------
   const legend = d3
     .select("#choropleth-legend")
     .append("svg")
-    .attr("width", 710)
-    .attr("height", 50);
+    .attr("width", wLeg)
+    .attr("height", hLeg);
 
-  // --------------------- Create Legend ---------------------------------------
+  // ----------------------- Construction Legend --------------------------------------
 
-  const linearScale = d3.scaleLinear().domain([0, 100]).range([0, 610]);
+  const percentageScale = d3
+    .scaleLinear()
+    .domain([0, maxPercentBachelor])
+    .range([0, 610]);
 
-  const quantizeScale = d3
+  const colorScale = d3
     .scaleQuantize()
-    .domain([0, 100])
-    .range(d3.schemeBlues[9]);
+    .domain([0, maxPercentBachelor])
+    .range(d3.schemeOranges[maxPercentBachelor / 10]);
 
-  const myData = d3.range(0, 100);
+  legend
+    .append("g")
+    .call(d3.axisBottom(percentageScale))
+    .attr("transform", "translate(50, 20)");
+
+  const myData = d3.range(0, maxPercentBachelor);
 
   legend
     .selectAll("rect")
     .data(myData)
     .join("rect")
-    .attr("x", (d) => linearScale(d))
-    .attr("width", 0)
-    .attr("height", 20)
-    .attr("width", 8)
-    .style("fill", (d) => quantizeScale(d));
+    .attr("x", (d) => percentageScale(d))
+    .attr("y", 5)
+    .attr("width", maxPercentBachelor / 10 + 1)
+    .attr("height", 15)
+    .attr("fill", (d) => colorScale(d))
+    .attr("transform", "translate(50, 0)");
+
+  legend.select("text").text("Percentage %");
 
   // ----------------------- Construction map ----------------------------------
 
@@ -62,7 +82,10 @@ function ready([us, edu]) {
     .enter()
     .append("path")
     .attr("d", path)
-    .attr("class", "counties");
+    .attr("class", "counties")
+    .style("fill", (d) =>
+      colorScale(edu.find((val) => val.fips === d.id).bachelorsOrHigher)
+    );
 
   svg
     .append("g")
